49d520c18c6d7fff503abee222969092e40d210f
charlottehartline <chartline@wisc.edu>
2024-11-14 14:10:57 -0600

    charlotte fragment layouts!

---
+++ app/src/main/java/com/cs407/locspend/LoginFragment.kt
@@ -0,0 +1,100 @@
+package com.cs407.locspend
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import java.security.MessageDigest
+
+class LoginFragment() : Fragment()  {
+
+    private lateinit var usernameEditText : EditText
+    private lateinit var passwordEditText : EditText
+    private lateinit var loginButton : Button
+    private lateinit var createAccount : TextView
+    private lateinit var errorText: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        usernameEditText = view.findViewById(R.id.usernameEditText)
+        passwordEditText = view.findViewById(R.id.passwordEditText)
+        loginButton = view.findViewById(R.id.loginButton)
+        createAccount = view.findViewById(R.id.createAccount)
+        errorText = view.findViewById(R.id.errorTextView)
+
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        usernameEditText.doAfterTextChanged {
+            errorText.visibility = View.GONE
+        }
+
+        passwordEditText.doAfterTextChanged {
+            errorText.visibility = View.GONE
+        }
+
+        // set the login button click action
+        loginButton.setOnClickListener {
+            // get the username and password from the EditTexts
+            val name = usernameEditText.text.toString()
+            val passwd = passwordEditText.text.toString()
+
+
+            // navigate to another fragment after successful login
+            lifecycleScope.launch {
+                val login_success = getUserPasswd(name, passwd)
+                if (login_success) {
+                    // navigate to the home page
+                } else {
+                    errorText.visibility = View.VISIBLE
+                }
+            }
+
+            // show an error message if either username or password is empty
+            if (name.isEmpty() || passwd.isEmpty()){
+                errorText.text = "Username/Password cannot be empty"
+                errorText.visibility = View.VISIBLE
+            }
+
+        }
+
+        createAccount.setOnClickListener{
+            lifecycleScope.launch {
+                //navigate to the create account page
+            }
+        }
+    }
+
+    private suspend fun getUserPasswd(
+        name: String,
+        passwdPlain: String
+    ): Boolean {
+        // hash the plain password
+        val hashedPasswd = hash(passwdPlain)
+
+        return true
+    }
+
+    private fun hash(input: String): String {
+        return MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+            .fold("") { str, it -> str + "%02x".format(it) }
+    }
+
+
+}
