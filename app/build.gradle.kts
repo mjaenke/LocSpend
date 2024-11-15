a19dd9064fbf3c6fa515906b13a11f0bdcfd4583
Morgan Jaenke <jaenke@wisc.edu>
2024-11-15 17:03:13 -0600

    Gets user's location, displays address on home fragment, updates marker on map.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,8 @@ android {
 }
 
 dependencies {
-
+    // https://mvnrepository.com/artifact/com.google.android.gms/play-services-location
+    implementation("com.google.android.gms:play-services-location:17.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
