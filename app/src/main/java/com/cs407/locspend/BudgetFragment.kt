eb537f324b466bce67b37eeff1136459cad7f9de
charlottehartline <chartline@wisc.edu>
2024-11-22 13:27:45 -0600

    charlotte dialog and budget calculations!

--- app/src/main/java/com/cs407/locspend/BudgetFragment.kt
+++ app/src/main/java/com/cs407/locspend/BudgetFragment.kt
@@ -1,10 +1,12 @@
 package com.cs407.locspend
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 
 /**
@@ -13,6 +15,11 @@ import android.widget.TextView
  * create an instance of this fragment.
  */
 class BudgetFragment : Fragment() {
+    lateinit var totalBudgetText : TextView
+    lateinit var totalSpentText : TextView
+    lateinit var totalRemainingText : TextView
+    lateinit var budgetPercentageText : TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +34,12 @@ class BudgetFragment : Fragment() {
         val budgetItemDining = view.findViewById<View>(R.id.budget_table_dining)
         val budgetItemGroceries = view.findViewById<View>(R.id.budget_table_groceries)
 
+        // Total budget/spent text views
+        totalBudgetText = view.findViewById<TextView>(R.id.total_budget)
+        totalSpentText = view.findViewById<TextView>(R.id.total_spent)
+        budgetPercentageText = view.findViewById<TextView>(R.id.percent_budget)
+        totalRemainingText = view.findViewById<TextView>(R.id.total_remaining)
+
         // Set values for "Dining" budget item
         budgetItemDining.findViewById<TextView>(R.id.category).text = "Dining"
         budgetItemDining.findViewById<TextView>(R.id.budget_value).text = "$125.00"
@@ -38,5 +51,95 @@ class BudgetFragment : Fragment() {
         budgetItemGroceries.findViewById<TextView>(R.id.budget_value).text = "$300.00"
         budgetItemGroceries.findViewById<TextView>(R.id.spent_value).text = "$150.00"
         budgetItemGroceries.findViewById<TextView>(R.id.remaining_value).text = "$150.00"
+
+
+        // Create onClickListener for the add spending button on each category
+        val categoryView = budgetItemDining
+        val addButton = categoryView.findViewById<TextView>(R.id.category)
+        addButton.setOnClickListener {
+            showDialog(categoryView)
+        }
+    }
+
+
+
+    private fun addSpending(
+        budgetTable: View,
+        amount: Int
+    ) {
+        //get budget table category
+        var category = budgetTable.findViewById<TextView>(R.id.category).text
+        val currentSpent = budgetTable.findViewById<TextView>(R.id.spent_value)
+        val currentRemaining = budgetTable.findViewById<TextView>(R.id.remaining_value)
+
+        // get the total budget for the category, amount spent, and current remaining
+        var categoryBudget = 100
+        var spent = stringToInt(currentSpent.text.toString())
+        var remaining = stringToInt(currentSpent.text.toString())
+
+        if (spent != null) {
+            spent = spent + amount
+        }
+
+        if (remaining != null) {
+            remaining = remaining - amount
+        }
+
+        //update this in the database
+
+        //put new values in the budget table
+        currentSpent.text = spent.toString()
+        currentRemaining.text = remaining.toString()
+
+    }
+
+    fun stringToInt(input: String): Int? {
+        return try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            null // Return null if the string cannot be converted
+        }
+    }
+
+    private fun showDialog(categoryView : View) {
+        val input = EditText(requireContext())
+        input.hint = "$0.00"
+        AlertDialog.Builder(requireContext())
+            .setMessage("Amount:")
+            .setView(input)
+            .setPositiveButton("Ok") {dialog,_ ->
+                val userInput = input.text.toString()
+                addSpending(categoryView, userInput.toInt())
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") {dialog,_ ->
+                dialog.dismiss()
+            }
+
+    }
+
+    private fun updateTotalBudget(){
+        val total = 100
+        val newSpent = 0
+        val newRemaining = 0
+        var newPercentage = 0
+
+        // need database to find real values!
+
+        if (total != 0){
+            newPercentage = (total - newRemaining)/total
+        } else {
+            newPercentage = 0
+        }
+        totalRemainingText.text = newRemaining.toString()
+        totalSpentText.text = newSpent.toString()
+        budgetPercentageText.text = newPercentage.toString()
+
+
+
+
+
+
     }
 }
+
