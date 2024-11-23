f0b7274731b7b307c72ad05791a98f368e8f3991
charlottehartline <chartline@wisc.edu>
2024-11-23 09:47:13 -0600

    Budget calculations using the database!

--- app/src/main/java/com/cs407/locspend/BudgetFragment.kt
+++ app/src/main/java/com/cs407/locspend/BudgetFragment.kt
@@ -8,9 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import com.cs407.locspend.data.BudgetDatabase
 import kotlinx.coroutines.launch
 
 /**
@@ -18,11 +20,44 @@ import kotlinx.coroutines.launch
  * Use the [BudgetFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BudgetFragment : Fragment() {
+class BudgetFragment (
+    private val injectedUserViewModel: UserViewModel? = null
+) : Fragment() {
     lateinit var totalBudgetText : TextView
     lateinit var totalSpentText : TextView
     lateinit var totalRemainingText : TextView
     lateinit var budgetPercentageText : TextView
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var budgetDB : BudgetDatabase
+    private lateinit var categories : List<String>
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        budgetDB = BudgetDatabase.getDatabase(requireContext())
+        userViewModel = if (injectedUserViewModel != null) {
+            injectedUserViewModel
+        } else {
+            ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
+        }
+        categories =
+            arrayOf("Dining", "Grocery", "Clothing","Transportation","Entertainment","Miscellaneous").toList()
+        val userState = userViewModel.userState.value
+        lifecycleScope.launch {
+            val countBudget = budgetDB.budgetDao().userBudgetCount(userState.id)
+            if (countBudget == 0) {
+                for (category in categories){
+                    budgetDB.budgetDao().upsertBudget(
+                        Budget(
+                            budgetCategory = category,
+                            budgetAmount = 0,
+                            budgetSpent = 0
+                        ), userState.id
+                    )
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +68,7 @@ class BudgetFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         // Find each included layout by its unique ID
         val budgetItemDining = view.findViewById<View>(R.id.budget_table_dining)
@@ -67,7 +103,7 @@ class BudgetFragment : Fragment() {
 
 
 
-    private fun addSpending(
+    private suspend fun addSpending(
         budgetTable: View,
         amount: Int
     ) {
@@ -75,25 +111,38 @@ class BudgetFragment : Fragment() {
         var category = budgetTable.findViewById<TextView>(R.id.category).text
         val currentSpent = budgetTable.findViewById<TextView>(R.id.spent_value)
         val currentRemaining = budgetTable.findViewById<TextView>(R.id.remaining_value)
+        val userState = userViewModel.userState.value
+        var total_budget = 0.0
+        var spent = 0.0
+        var remaining = 0.0
 
         // get the total budget for the category, amount spent, and current remaining
-        var categoryBudget = 100
-        var spent = stringToInt(currentSpent.text.toString())
-        var remaining = stringToInt(currentSpent.text.toString())
+        lifecycleScope.launch {
+            val budget = budgetDB.budgetDao().getByCategory(category.toString())
+            total_budget = budget.budgetAmount
+            spent = budget.budgetSpent
+            var id = budget.budgetId
 
-        if (spent != null) {
-            spent = spent + amount
+            if (spent != null) {
+                spent = spent + amount
+                budgetDB.budgetDao().upsertBudget(Budget(
+                    budgetCategory = category,
+                    budgetAmount = total_budget,
+                    budgetSpent = spent
+                ), userState.id
+                )
+            }
+
+            if (remaining != null) {
+                remaining = total_budget - spent
+            }
+
         }
-
-        if (remaining != null) {
-            remaining = remaining - amount
-        }
-
-        //update this in the database
 
         //put new values in the budget table
         currentSpent.text = spent.toString()
         currentRemaining.text = remaining.toString()
+        updateTotalBudget()
 
     }
 
@@ -122,26 +171,32 @@ class BudgetFragment : Fragment() {
 
     }
 
-    private fun updateTotalBudget(){
-        val total = 100
-        val newSpent = 0
-        val newRemaining = 0
-        var newPercentage = 0
+    private suspend fun updateTotalBudget(){
+        var totalBudget = 0.0
+        var totalSpent = 0.0
+        var newRemaining = 0.0
+        var newPercentage = 0.0
 
-        // need database to find real values!
+        val userState = userViewModel.userState.value
 
-        if (total != 0){
-            newPercentage = (total - newRemaining)/total
-        } else {
-            newPercentage = 0
+        // add up all budget/spent totals from the database
+        val budgets = budgetDB.deleteDao().getAllBudgetIdsByUser(userState.id)
+        for (budget in budgets) {
+            val current_budget = budgetDB.budgetDao().getById(budget)
+            totalBudget += current_budget.budgetAmount
+            totalSpent += current_budget.budgetSpent
         }
+
+        if (totalBudget != 0.0){
+            newPercentage = (totalBudget - totalSpent)/totalBudget
+        } else {
+            newPercentage = 0.0
+        }
+        newRemaining = totalBudget - totalSpent
+
         totalRemainingText.text = newRemaining.toString()
-        totalSpentText.text = newSpent.toString()
+        totalSpentText.text = totalSpent.toString()
         budgetPercentageText.text = newPercentage.toString()
-
-
-
-
 
 
     }
